7c0760d73714b16a87e22f316e167977a7834f7c
Sergey <denisenko.sa@ffx.media>
2024-02-29 18:48:45 +0300

    исправление опечатки

--- Seminar5/java/MVCStudentApp/src/Controller/Interfaces/iGetView.java
+++ Seminar5/java/MVCStudentApp/src/Controller/Interfaces/iGetView.java
@@ -5,7 +5,25 @@ import java.util.List;
 import Model.Domain.Student;
 
 public interface iGetView {
+    /**
+     * @apiNote Выводит в консоль список студентов
+     * @param students Список студентов
+     * @return void
+     */
     public void printAllStudents(List<Student> students);
+
+    /**
+     * @apiNote Читает строку из консоли
+     * @param msg Информационное сообщение
+     * @return String
+     */
     public String prompt(String msg);
-    public void infoDelete(boolean isDelete, String idStudent)
+
+    /**
+     * @apiNote Выводит текстовую информацию о статусе удаления студента из списка
+     * @param isDelete Статус удаления
+     * @param idStudent Идентификатор студента
+     * @return void
+     */
+    public void infoDelete(boolean isDelete, String idStudent);
 }
